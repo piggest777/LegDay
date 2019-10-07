4b9fff60c65bddc414cebf8ffecd8b16fc624c15
Denis Rakitin <piggest@mail.ru>
2019-10-07 11:55:12 +0200

    Initial Commit

---
+++ LegDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LegDay
+//
+//  Created by Denis Rakitin on 2019-10-07.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
